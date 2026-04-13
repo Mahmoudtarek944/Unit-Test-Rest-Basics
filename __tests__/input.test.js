ab74404613b0ff9608ac0edc19e0c9d40f1edf1d
Mahmoud Tarek <mahmod122tarek@gmail.com>
2026-04-13 18:25:30 +0200

    adding first unit test input name

---
+++ __tests__/input.test.js
@@ -0,0 +1,22 @@
+const nameTest = require("../input");
+
+describe("Testing On The Name ", () => {
+  test("Should Return The Same Name If Valid", () => {
+    expect(nameTest("Mahmoud")).toBe("Mahmoud");
+  });
+  test("Should Return 'Unknown' If Name Is undefined", () => {
+    expect(nameTest()).toBe("Unknown");
+  });
+  test("Should Return 'Unknown' If Name Is Empty", () => {
+    expect(nameTest("")).toBe("Unknown");
+  });
+  test("Should Remove Leading Underscores", () => {
+    expect(nameTest("_Mahmoud")).toBe("Mahmoud");
+  });
+  test("Should Truncate Name If Longer Than 10 Characters", () => {
+    expect(nameTest("Mahmoud Tarek Eid")).toBe("Mahmoud Ta");
+  });
+  test("Should Remove Underscores Then Truncate If Needed", () => {
+    expect(nameTest("_MahmoudTarekEid")).toBe("MahmoudTar");
+  });
+});
---
+++ input.js
@@ -0,0 +1,16 @@
+function testName(name) {
+  if (name === undefined || name.length === 0) {
+    return "Unknown";
+  }
+  if (name.startsWith("_")) {
+    while (name.startsWith("_")) {
+      name = name.substring(1);
+    }
+  } // Not use else if because if after ignore _ from the name , the length of name also greater then 10 char
+  if (name.length > 10) {
+    name = name.substring(0, 10);
+  }
+
+  return name;
+}
+module.exports = testName;
