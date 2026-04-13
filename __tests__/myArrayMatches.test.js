0a508d09bc65cb40050b7c8f67b39ae63f23135b
Mahmoud Tarek <mahmod122tarek@gmail.com>
2026-04-13 21:46:16 +0200

    add remind matches

--- __tests__/myArrayMatches.test.js
+++ __tests__/myArrayMatches.test.js
@@ -13,7 +13,36 @@ describe("Testing On Array My Matches", () => {
   test("Should Not Have 40 As First Element", () => {
     expect(myArr[0]).toBe(10);
   });
-  test("should match exact array", () => {
+  test("Should Match Exact Array", () => {
     expect(myArr).toEqual([10, 20, 30]);
+  });
+
+  test("Should All is Number", () => {
+    for (let i = 0; i < myArr.length; i++) {
+      expect(isNaN(myArr[i])).toBeFalsy();
+    }
+  });
+  test("Should All is Number Again", () => {
+    for (let i = 0; i < myArr.length; i++) {
+      expect(isNaN(myArr[i])).not.toBeTruthy();
+    }
+  });
+
+  test("Should First Element Greater 5", () => {
+    expect(myArr[0]).toBeGreaterThan(5);
+  });
+  test("Should First Element Less 50", () => {
+    expect(myArr[0]).toBeLessThan(50);
+  });
+
+  let obj = {
+    name: "Mahmoud",
+    age: 20,
+  };
+  test("Should Have Name Property", () => {
+    expect(obj).toHaveProperty("name");
+  });
+  test("Should Have Name Property Mahmoud", () => {
+    expect(obj).toHaveProperty("name", "Mahmoud");
   });
 });
